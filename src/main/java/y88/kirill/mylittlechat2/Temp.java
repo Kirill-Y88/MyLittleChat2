d088f1f90c47787c7ca542bdbc0805bd7cc4bb35
KirillPushkin <benberov09@mail.ru>
2022-08-07 21:14:27 +0300

    commit 2

---
+++ src/main/java/y88/kirill/mylittlechat2/Temp.java
@@ -0,0 +1,13 @@
+package y88.kirill.mylittlechat2;
+
+
+import com.hummeling.if97.IF97;
+
+public class Temp {
+    public static void main(String[] args) {
+
+        IF97 if97 = new IF97(IF97.UnitSystem.ENGINEERING);
+        System.out.println(if97.specificVolumePH( 5, 1500));
+
+    }
+}
